b4f8556e0759b709dbb3eca64e063adc715c7dfe
Andrew Bolin <andrew.bolin@csiro.au>
2019-12-06 12:10:16 +1100

    refining tests

--- docker/test-harness/01_initial_tests.py
+++ docker/test-harness/01_initial_tests.py
@@ -1,4 +1,5 @@
 import tango
+import unittest
 
 _my_server = "TangoSnmp"
 
@@ -8,40 +9,57 @@ def find_server():
     print("Found {} instances of {}".format(len(sl), _my_server))
     assert len(sl) > 0
 
-def test_test_device_is_running():
-    tango_test = tango.DeviceProxy("ab/ab/simulator")
-    print("tango_test device state: {}".format(tango_test.state()))
-    assert True
+def not_exported_device():
+    not_exp = tango.DeviceProxy("not/exported/device")
+    print("not_exp device state: {}".format(not_exp.state()))
+    print("not_exp device status: {}".format(not_exp.status()))
 
-def test_snmp_simulator():
-    snmp_sim = tango.DeviceProxy("ab/ab/simulator")
-    print("snmp_sim attributes (pre-init):")
-    for a in snmp_sim.attribute_list_query():
-        print(a.name)
-    print("")
-    print("snmp_sim DynamicAttributes:")
-    for key,val in snmp_sim.get_property('DynamicAttributes').items():    
-        print(key, val)
-    print("")
-    print("snmp_sim state: {}".format(snmp_sim.state()))
-    print("snmp_sim status: {}".format(snmp_sim.status()))
-    
-    print("")
-    print("sending init cmd")
-    snmp_sim.init()
-    print("snmp_sim attributes :")
-    for a in snmp_sim.attribute_list_query():
-        print(a.name)
-    print("")
-    #  note for interactive debugging:
-    #  snmp_sim.put_property({'DynamicAttributes': ['one=SIMULATOR-MIB:simulator.1.1,devlong,READ,one thing,units,%d']})
-    print("snmp_sim.one: {}".format(snmp_sim.one))
-    print("setting to 84")
-    snmp_sim.one = 84    
-    print("snmp_sim.one: {}".format(snmp_sim.one))
-    assert True
+def no_comms_device_state():
+    no_comms = tango.DeviceProxy("no/comms/device")
+    return no_comms.state()
+
+
+    # TODO - refactor to use testing framework properly (Check what other SKA tango uses!)
+    # - a device in its own SimulationMode (no SNMP)
+
+class MyTestCase(unittest.TestCase):
+    def test00(self):
+        db = tango.Database()
+        sl = db.get_server_list("{}*".format(_my_server))
+        self.assertGreater(len(sl), 0, 'TangoSnmp instances in db')
+
+    def test10(self):
+        self.assertRaises(tango.ConnectionFailed, not_exported_device)
+    def test11(self):
+        no_comms = tango.DeviceProxy("no/comms/device")
+        no_comms.init()
+        self.assertEqual(no_comms.state(), tango.DevState.ON, 'no comms initial state')
+        print('no comms device state after init: {}'.format(no_comms.state()))
+    def test12(self):
+        no_comms = tango.DeviceProxy("no/comms/device")
+        with self.assertRaises(tango.DevFailed) as context:
+            x = no_comms.none
+        e = context.exception
+        # DevFailed exception should contain two 'DevError's
+        # args[0] from TangoSnmp (timeout)
+        # args[1] from DeviceProxy (can't read attribute)
+        self.assertEqual(e.args[0].reason, 'TangoSnmp_SnmpError', 'DevError reason 0')
+        self.assertEqual(e.args[1].reason, 'API_AttributeFailed', 'DevError reason 1')
+        print('no comms device state after attr fail: {}'.format(no_comms.state()))
+        print('no comms device status after attr fail: {}'.format(no_comms.status()))
+        # TODO - test an attribute that is not even defined in DynamicAttributes,
+        # see if result is any different to above...
+
+    def test20(self):
+        snmp_sim = tango.DeviceProxy("ab/ab/simulator")
+        self.assertEqual(snmp_sim.state(), tango.DevState.ON, 'SNMP sim device ON')
+    def test21(self):
+        snmp_sim = tango.DeviceProxy("ab/ab/simulator")
+        self.assertEqual(snmp_sim.one, 42, 'SNMP sim read')
+    def test22(self):
+        snmp_sim = tango.DeviceProxy("ab/ab/simulator")
+        snmp_sim.one = 84    
+        self.assertEqual(snmp_sim.one, 84, 'SNMP sim write')
 
 if __name__ == '__main__':
-    find_server()
-    test_test_device_is_running()
-    test_snmp_simulator()
+    unittest.main()
