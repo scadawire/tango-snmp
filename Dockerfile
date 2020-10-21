d57e4c67a3ff4b70ea276e06fbbdacb5b4e4ee56
andrew-bolin <fivepointpalmexplodingheart+github@gmail.com>
2020-10-21 23:03:45 +0000

    AT6-670 Migrate CI to k8s

---
+++ Dockerfile
@@ -0,0 +1,21 @@
+ARG DOCKER_REGISTRY_USER
+ARG DOCKER_REGISTRY_HOST
+#FROM ${DOCKER_REGISTRY_HOST}/${DOCKER_REGISTRY_USER}/tangosnmp-builder:latest as buildenv
+FROM ${DOCKER_REGISTRY_HOST}/${DOCKER_REGISTRY_USER}/ska-python-buildenv:9.3.1.1 as buildenv
+FROM ${DOCKER_REGISTRY_HOST}/${DOCKER_REGISTRY_USER}/ska-python-runtime:9.3.1.1
+
+USER root
+# snmp-mibs-downloader is in the non-free repo, so we add it to the sources.list
+RUN cat /etc/apt/sources.list | sed 's/main$/main contrib non-free/' > /etc/apt/sources.list_new && \
+    mv /etc/apt/sources.list_new /etc/apt/sources.list && \
+    export DEBIAN_FRONTEND=noninteractive && apt update && \
+    apt install -y libsnmp-dev snmp-mibs-downloader && \
+    apt clean && rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
+COPY build/bin/TangoSnmp /usr/local/bin
+
+# this should install snmpsim, for use in testing
+RUN pip install -e . --extra-index-url https://nexus.engageska-portugal.pt/repository/pypi/simple
+
+USER tango
+ENTRYPOINT ["/app/entrypoint.sh"]
+CMD ["TangoSnmp", "TangoSnmp-default-instance"]
---
+++ post-deployment/tests/test_01.py
@@ -0,0 +1,89 @@
+import pytest
+import tango
+from tango import Database, DeviceProxy
+
+
+@pytest.fixture(scope="class")
+def no_comms():
+    dev = DeviceProxy("no/comms/device")
+    return dev
+
+
+@pytest.fixture(scope="class")
+def simulator():
+    sim = DeviceProxy("ab/ab/simulator")
+    return sim
+
+
+class TestDb:
+    def test_devices_in_db(self):
+        _my_server = "TangoSnmp"
+        db = tango.Database()
+        sl = db.get_server_list("{}*".format(_my_server))
+        assert len(sl) > 0
+
+    def test_not_exported(self):
+        not_exp = tango.DeviceProxy("not/exported/device")
+        with pytest.raises(tango.ConnectionFailed):
+            x = not_exp.state()
+
+
+@pytest.mark.usefixtures("no_comms")
+class TestNoComms:
+    def test_init(self, no_comms):
+        no_comms.init()
+        assert no_comms.state() == tango.DevState.ON
+
+    def test_attr_fail(self, no_comms):
+        with pytest.raises(tango.DevFailed) as e:
+            # this attribute is specified in the DynamicAttributes
+            x = no_comms.one
+        # DevFailed exception should contain two 'DevError's
+        # args[0] from TangoSnmp (timeout)
+        # args[1] from DeviceProxy (can't read attribute)
+        assert e.value.args[0].reason == "TangoSnmp_SnmpError"
+        assert e.value.args[1].reason == "API_AttributeFailed"
+        assert no_comms.state() == tango.DevState.ON
+
+    def test_attr_not_exist(self, no_comms):
+        with pytest.raises(AttributeError) as e:
+            # this attribute is NOT specified in the DynamicAttributes
+            x = no_comms.attribute_that_doesnt_exist
+        assert no_comms.state() == tango.DevState.ON
+
+    def test_simulation(self, no_comms):
+        no_comms.GlobalSimulationEnable(True)
+        # in simulation mode, returned value increments by 10 each time
+        assert no_comms.one == 10
+        assert no_comms.one == 20
+        assert no_comms.one == 30
+
+
+@pytest.mark.usefixtures("simulator")
+class TestSnmpSimulatorComms:
+    def test_on(self, simulator):
+        assert simulator.state() == tango.DevState.ON
+
+    def test_snmp_read(self, simulator):
+        # starting value of 42 is specified in one.snmprec config file
+        assert simulator.one == 42
+
+    def test_simulation(self, simulator):
+        # this is the Tango device simulation
+        # (i.e. it does not communicate over SNMP)
+        simulator.GlobalSimulationEnable(True)
+        assert simulator.one == 10
+        simulator.GlobalSimulationEnable(False)
+        assert simulator.one == 42
+
+    def test_sim_ignores_write(self, simulator):
+        simulator.GlobalSimulationEnable(True)
+        simulator.one = 888
+        assert simulator.one != 888
+        simulator.GlobalSimulationEnable(False)
+        assert simulator.one == 42
+
+    def test_snmp_write(self, simulator):
+        # this write should go to the SNMP protocol simulator and then be read back
+        simulator.one = 84
+        assert simulator.one == 84
